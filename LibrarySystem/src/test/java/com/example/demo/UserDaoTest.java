c2d07173e2bd2c536b611f952eff141a4ceaa81e
takoNkmt01 <0cto.pass0512@gmail.com>
2019-07-26 18:18:53 +0900

    修正反映

--- LibrarySystem/src/test/java/com/example/demo/UserDaoTest.java
+++ LibrarySystem/src/test/java/com/example/demo/UserDaoTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +21,12 @@ public class UserDaoTest {
 
 	@Test
 	public void countTest() {
-		assertEquals(userMapper.count(), 8);
+		assertEquals(userMapper.count(), 20);
 	}
 
-
+	@Test
+	@Sql("/testdata.sql")
+	public void countTest2() {
+		assertEquals(userMapper.count(), 21);
+	}
 }
---
+++ LibrarySystem/src/test/resources/testdata.sql
@@ -0,0 +1,3 @@
+/*テスト用データ*/
+insert into member (member_id, password, member_name, birthday, address, tel_number, mail_address, admin_flg, create_date)
+values('RS999998','$2a$10$wskaPfbn8H7Pe1uUpKbbTeiOBNw0qhYeoPFOxsSI6lguc0roj7Oye','テスト太郎','1111/11/11','東京','123456','aaa@aa.jp','ADMIN','2019/07/26');
